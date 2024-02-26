7e93825bf3a2b96d13e61644ec386cdc12493961
OgyCan8163 <62715950+OgyCan8163@users.noreply.github.com>
2024-02-25 22:59:35 -0500

    fix error

--- application.py
+++ application.py
@@ -48,9 +48,6 @@ def submit():
     answer = 'this response is still under development.'
     return jsonify({'answer': answer})
 
-
-
-
 if __name__ == '__main__':
     application.run(debug=True)
 
